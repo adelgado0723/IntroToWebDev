0be108d8d6d74d4d5ae944491dd953ec2096cf14
Andres Delgado <adelgado0723@gmail.com>
2019-05-01 23:38:01 -0400

    Implemented breed-selector functionality using the dog.ceo API

--- Ajax Requests/doggos.js
+++ Ajax Requests/doggos.js
@@ -3,7 +3,6 @@ const BREED_LIST_URL = 'https://dog.ceo/api/breeds/list/all';
 const doggos = document.querySelector('.doggos');
 const addDoggoButton = document.querySelector('.add-doggo');
 const controls = document.querySelector('.controls');
-const breedList = [];
 let breeds = [];
 
 const select = document.createElement('select');
@@ -16,60 +15,72 @@ controls.appendChild(select);
 // Request list of dog breeds
 const breedListPromise = fetch(BREED_LIST_URL);
 breedListPromise
-  .then(response => {
-    // "then" waits for the response before proceeding
-    // A promise is returned from the .json() function
-    // because it can take some time to parse the JSON
-    const processingPromise = response.json();
-    return processingPromise;
-  })
-  .then(processedResponse => {
-    // Second then is to wait on the JSON parsing
-    breeds = processedResponse.message;
-    for (let breed in breeds) {
-      if (breeds[breed].length > 0) {
-        breeds[breed].forEach(breed => {
-          const name = breed + ' ' + type;
-          const value = breed.type.toLowerCase() + '-' + breed.breed.toLowerCase();
-          const breedOption = document.createElement('option');
-          option.value = value;
-          option.innerHTML = name;
-          select.appendChild(breedOption);
-        });
-      } else {
-        const option = document.createElement('option');
-        option.value = breed.toLowerCase();
-        option.innerHTML = breeds.breed;
-        select.appendChild(option);
-      }
-    }
-  });
-
-console.log(breeds.length);
-console.log(breeds['bulldog']);
-
-console.log(breeds);
-console.log(select);
-
-function addDoggo() {
-  const promise = fetch(DOG_URL);
-  promise
     .then(response => {
-      // "then" waits for the response before proceeding
-
-      // A promise is returned from the .json() function
-      // because it can take some time to parse the JSON
-      const processingPromise = response.json();
-      return processingPromise;
+        // "then" waits for the response before proceeding
+        // A promise is returned from the .json() function
+        // because it can take some time to parse the JSON
+        const processingPromise = response.json();
+        return processingPromise;
     })
     .then(processedResponse => {
-      // Second then is to wait on the JSON parsing
-      console.log(processedResponse);
-      const img = document.createElement('img');
-      img.src = processedResponse.message;
-      img.alt = 'Cute doggos';
-      doggos.appendChild(img);
+        // Second then is to wait on the JSON parsing
+        breeds = processedResponse.message;
+        // console.log(breeds);
+        const properties = Object.keys(breeds);
+        const breedList = [];
+
+        properties.forEach(property => {
+            const varieties = breeds[property];
+            const varietyKeys = Object.keys(varieties);
+            const numVarieties = varietyKeys.length;
+            let name = '';
+            let value = '';
+
+            if (numVarieties > 0) {
+                varieties.forEach(variety => {
+                    name = `${variety} ${property}`;
+                    value = `${property}-${variety}`;
+                    breedList.push({ name, value });
+                });
+            } else {
+                name = property;
+                value = property;
+                breedList.push({ name, value });
+            }
+        });
+        breedList.forEach(dog => {
+            const varietyOption = document.createElement('option');
+            varietyOption.value = dog.value;
+            varietyOption.innerHTML = dog.name;
+            select.appendChild(varietyOption);
+        });
     });
+
+function addDoggo() {
+    let promise;
+    const selection = select.options[select.selectedIndex].value;
+    if (selection === 'any-breed') {
+        promise = fetch(DOG_URL);
+    } else {
+        promise = fetch(`https://dog.ceo/api/breed/${selection}/images/random`);
+    }
+    promise
+        .then(response => {
+            // "then" waits for the response before proceeding
+
+            // A promise is returned from the .json() function
+            // because it can take some time to parse the JSON
+            const processingPromise = response.json();
+            return processingPromise;
+        })
+        .then(processedResponse => {
+            // Second then is to wait on the JSON parsing
+            console.log(processedResponse);
+            const img = document.createElement('img');
+            img.src = processedResponse.message;
+            img.alt = 'Cute doggos';
+            doggos.appendChild(img);
+        });
 }
 console.log('This will log first while the AJAX request is being made');
 addDoggoButton.addEventListener('click', addDoggo);
