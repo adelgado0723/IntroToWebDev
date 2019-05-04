236cafc3b57cef7fddf20e0917312740f93d2524
Andres Delgado <adelgado0723@gmail.com>
2019-05-04 16:56:42 -0400

    Fixed back button

--- Project2 - Calculator/scripts.js
+++ Project2 - Calculator/scripts.js
@@ -8,180 +8,171 @@ let cursorBlinkInterval;
 let doneBlinking = true;
 
 const operation = {
-  operandOne: undefined,
-  operator: ''
+    operandOne: undefined,
+    operator: '',
 };
 
 function blink() {
-  if (!cursor.classList.contains('hide')) {
-    cursor.classList.add('hide');
-  } else {
-    cursor.classList.remove('hide');
-  }
-  if (doneBlinking) {
-    clearInterval(cursorBlinkInterval);
-    if (cursor.classList.contains('hide')) {
-      cursor.classList.remove('hide');
+    if (!cursor.classList.contains('hide')) {
+        cursor.classList.add('hide');
+    } else {
+        cursor.classList.remove('hide');
     }
-  }
+    if (doneBlinking) {
+        clearInterval(cursorBlinkInterval);
+        if (cursor.classList.contains('hide')) {
+            cursor.classList.remove('hide');
+        }
+    }
 }
 
 function blinkCursor() {
-  cursorBlinkInterval = setInterval(blink, 540);
+    cursorBlinkInterval = setInterval(blink, 540);
 }
 function executeOperation(operator) {
-  let result = input.innerHTML;
-  let parsed = parseInt(result);
-  const hasNoOperand = isNaN(operation.operandOne);
+    const result = input.innerHTML;
+    const parsed = parseInt(result);
+    const hasNoOperand = isNaN(operation.operandOne);
 
-  switch (operator) {
-    case '+':
-      if (hasNoOperand) {
-        operation.operandOne = parsed;
-      } else {
-        operation.operandOne += parsed;
-        input.innerHTML = operation.operandOne.toString();
-      }
-      isDisplayingResult = true;
+    switch (operator) {
+        case '+':
+            if (hasNoOperand) {
+                operation.operandOne = parsed;
+            } else {
+                operation.operandOne += parsed;
+                input.innerHTML = operation.operandOne.toString();
+            }
+            isDisplayingResult = true;
 
-      break;
+            break;
 
-    case '-':
-      if (hasNoOperand) {
-        operation.operandOne = parsed;
-      } else {
-        operation.operandOne -= parsed;
-        input.innerHTML = operation.operandOne.toString();
-      }
-      isDisplayingResult = true;
-      break;
+        case '-':
+            if (hasNoOperand) {
+                operation.operandOne = parsed;
+            } else {
+                operation.operandOne -= parsed;
+                input.innerHTML = operation.operandOne.toString();
+            }
+            isDisplayingResult = true;
+            break;
 
-    case '←':
-      // Remove the last digit of the number in the display.
-      // If the last digit is deleted, set result to zero.
-      input.innerHTML = result.length > 1 ? result.substr(0, result.length - 1) : '0';
-      break;
+        case '←':
+            // Remove the last digit of the number in the display.
+            // If the last digit is deleted, set result to zero.
+            input.innerHTML = result.length > 1 ? result.substr(0, result.length - 1) : '0';
+            break;
 
-    case 'C':
-      // Clear values in operand and sets result to
-      input.innerHTML = 0;
-      clearOperation();
-      isDisplayingResult = true;
-      break;
+        case 'C':
+            // Clear values in operand and sets result to
+            input.innerHTML = 0;
+            clearOperation();
+            isDisplayingResult = true;
+            break;
 
-    case '÷':
-      if (hasNoOperand) {
-        operation.operandOne = parsed;
-      } else {
-        operation.operandOne /= parsed;
-        input.innerHTML = operation.operandOne.toString();
-      }
-      isDisplayingResult = true;
-      break;
-    // TODO: Might want to handle '/' operator for keyboard input
+        case '÷':
+            if (hasNoOperand) {
+                operation.operandOne = parsed;
+            } else {
+                operation.operandOne /= parsed;
+                input.innerHTML = operation.operandOne.toString();
+            }
+            isDisplayingResult = true;
+            break;
+        // TODO: Might want to handle '/' operator for keyboard input
 
-    case '×':
-      if (hasNoOperand) {
-        operation.operandOne = parsed;
-      } else {
-        operation.operandOne *= parsed;
-        input.innerHTML = operation.operandOne.toString();
-      }
-      isDisplayingResult = true;
-      break;
+        case '×':
+            if (hasNoOperand) {
+                operation.operandOne = parsed;
+            } else {
+                operation.operandOne *= parsed;
+                input.innerHTML = operation.operandOne.toString();
+            }
+            isDisplayingResult = true;
+            break;
 
-    case '=':
-      if (hasNoOperand) {
-        operation.operandOne = parsed;
-      } else {
-        executeOperation(operation.operator);
-        clearOperation();
-      }
+        case '=':
+            if (hasNoOperand) {
+                operation.operandOne = parsed;
+            } else {
+                executeOperation(operation.operator);
+                clearOperation();
+            }
 
-      break;
+            break;
 
-    default:
-      console.log(`WARNING: Attempting to run invalid operation: ${operation}`);
-      break;
-  }
-  return result;
+        default:
+            console.log(`WARNING: Attempting to run invalid operation: ${operation}`);
+            break;
+    }
+    return result;
 }
 function clearOperation() {
-  operation.operandOne = undefined;
-  operation.operator = '';
+    operation.operandOne = undefined;
+    operation.operator = '';
 }
 
 function isOperator(char) {
-  if (
-    char === '+' ||
-    char === '-' ||
-    char === '÷' ||
-    char === '×' ||
-    char === '←' ||
-    char === 'C' ||
-    char === '='
-  ) {
-    return true;
-  } else {
+    if (char === '+' || char === '-' || char === '÷' || char === '×' || char === '←' || char === 'C' || char === '=') {
+        return true;
+    }
     return false;
-  }
 }
 function getOperatorType(char) {
-  if (char === '+' || char === '-' || char === '÷' || char === '×') {
-    return 'Binary';
-  } else if (char === '←' || char === 'C') {
-    return 'Unary';
-  } else {
+    if (char === '+' || char === '-' || char === '÷' || char === '×') {
+        return 'Binary';
+    }
+    if (char === '←' || char === 'C') {
+        return 'Unary';
+    }
     return 'Not An Operator';
-  }
 }
 function handleClick(e) {
-  console.log(this.innerHTML);
-  const keyClicked = this.innerHTML;
-  evaluateOperation(keyClicked);
+    console.log(this.innerHTML);
+    const keyClicked = this.innerHTML;
+    evaluateOperation(keyClicked);
 }
 
 function evaluateOperation(key) {
-  const parsedInput = parseInt(key);
-  if (!isNaN(parsedInput)) {
-    // key is a number
-    if (isDisplayingResult || input.innerHTML === '0') {
-      input.innerHTML = key;
-      isDisplayingResult = false;
+    const parsedInput = parseInt(key);
+    if (!isNaN(parsedInput)) {
+        // key is a number
+        if (isDisplayingResult || input.innerHTML === '0') {
+            input.innerHTML = key;
+            isDisplayingResult = false;
+        } else {
+            input.innerHTML += key;
+        }
     } else {
-      input.innerHTML += key;
+        // key is not a number
+        if (key !== '←' && key !== 'C' && key !== '=') {
+            operation.operator = key;
+        }
+        executeOperation(key);
+        console.log(`OperandOne = ${operation.operandOne}`);
     }
-  } else {
-    // key is not a number
-    if (key !== '←' && key !== 'C' && key !== '=') {
-      operation.operator = key;
-    }
-    executeOperation(key);
-    console.log('OperandOne = ' + operation.operandOne);
-  }
 }
 
 function handleKeyDown(e) {
-  const keyPressed = String.fromCharCode(e.keyCode);
-  console.log(keyPressed);
-  // Validate input and replace operator symbols with
-  // ones we are expecting.
-  evaluateOperation(keyPressed);
+    const keyPressed = String.fromCharCode(e.keyCode);
+    console.log(keyPressed);
+    // Validate input and replace operator symbols with
+    // ones we are expecting.
+    evaluateOperation(keyPressed);
 }
 keys.forEach(key => {
-  key.addEventListener('click', handleClick);
+    key.addEventListener('click', handleClick);
 });
 window.addEventListener('keydown', handleKeyDown);
 
 // Blinking cursor when display has focus
 window.addEventListener('click', function(e) {
-  if (calc.contains(e.target)) {
-    if (doneBlinking) {
-      blinkCursor();
-      doneBlinking = false;
+    if (calc.contains(e.target)) {
+        if (doneBlinking) {
+            blinkCursor();
+            doneBlinking = false;
+        }
+    } else {
+        // Clicked outside the box
+        doneBlinking = true;
     }
-  } else {
-    // Clicked outside the box
-    doneBlinking = true;
-  }
 });
