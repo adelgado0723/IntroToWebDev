fb05f0e9f6f9d2a66ccb9ebf8f61b5b3be886779
Andres Delgado <adelgado0723@gmail.com>
2019-05-08 01:44:59 -0400

    Continued working on Projet 5

--- Project5_NodeServer/server.js
+++ Project5_NodeServer/server.js
@@ -21,7 +21,7 @@ function getRandomComplement() {
 const app = express();
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html')).end();
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/complement', (req, res) => {
