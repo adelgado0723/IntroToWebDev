33451dca0bedc48d6182733467d0d93f93337183
Andres Delgado <adelgado0723@gmail.com>
2019-05-08 01:16:16 -0400

    Worked on Project 5

--- Project5_NodeServer/server.js
+++ Project5_NodeServer/server.js
@@ -1,12 +1,35 @@
 const express = require('express');
+const path = require('path');
 
-const app = express;
+const complements = [
+    'You like nice today',
+    'That dress looks nice on you',
+    'Have you been working out?',
+    'You can do hard things',
+    "You've gotten far in this course. You're really smart",
+    "You're programming! How cool is that?",
+    "I'm really proud of you",
+    'You made this',
+    "You've learned a lot of things, and that's pretty hard to do",
+];
+
+function getRandomComplement() {
+    const randomIndex = Math.floor(Math.random() * complements.length);
+    return complements[randomIndex];
+}
+
+const app = express();
+
 app.get('/', (req, res) => {
-    res.end('Welcome to my site!');
+    res.sendFile(path.join(__dirname, 'index.html')).end();
 });
+
 app.get('/complement', (req, res) => {
-    res.end('You look very nice today.');
+    res.json({ complement: getRandomComplement() }).end();
 });
+
+app.use('/public', express.static('./public'));
+
 app.listen(3000);
 console.log('listening on http://localhost:3000');
 
